91fe613603c5567c140232165209335fca097e5e
alcb7 <canbulan1849@gmail.com>
2024-02-22 21:01:21 +0300

    entity

---
+++ OA.ETicaret/Core/OA.ETicaret.Domain/Entities/Customer.cs
@@ -0,0 +1,18 @@
+﻿using OA.ETicaret.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.ETicaret.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
---
+++ OA.ETicaret/Core/OA.ETicaret.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using OA.ETicaret.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.ETicaret.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+       public  ICollection<Order> Orders { get; set; } 
+
+    }
+}
