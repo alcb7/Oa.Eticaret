e9cbfa567e7bda27e9b6c2c21b4d68a34e2fe510
alcb7 <canbulan1849@gmail.com>
2024-07-23 01:55:34 +0300

    DB Eklendi

---
+++ OA.ETicaret/Infrastructure/OA.ETicaret.Persistance/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OA.ETicaret.Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.ETicaret.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceService(this IServiceCollection services)
+        {
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ETicaretAPIDb;"));
+        }
+    }
+}
